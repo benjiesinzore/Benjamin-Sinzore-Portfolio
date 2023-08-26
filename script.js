95433f7ad31489b8931a7b4ccff092fe24df5bc4
Benjamin Sinzore <benjaminsinzore@gmail.com>
2023-08-27 00:59:24 +0300

    Completed Navigation
    Completed working on all the Navigation, both for small screens and large screens.

--- script.js
+++ script.js
@@ -8,9 +8,25 @@ window.addEventListener("load", function () {
         splash.style.opacity = 0;
         main.style.display = "block";
 
+        // Allow scrolling by removing the no-scroll class from the body
+        document.body.classList.remove("no-scroll");
+
         
         setTimeout(function () {
             splash.remove();
         }, 1000);
-    }, 5000); 
+    }, 3000); 
 });
+
+
+
+//Handle Hamburger Manu onClick
+function toggleMenu() {
+
+    const menu = document.querySelector(".menu-links"); 
+    const icon = document.querySelector(".hamburger-icon"); 
+
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
